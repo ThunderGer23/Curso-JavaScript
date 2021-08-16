9f344e5fc8902624c2c6fc135a0c07bef5600785
ThunderGer <66645071+ThunderGer23@users.noreply.github.com>
2021-08-16 15:58:19 -0500

    Update 6.Funciones.js

--- 1.Clases/6.Funciones.js
+++ 1.Clases/6.Funciones.js
@@ -1,3 +1,9 @@
 /*
+*   Funciones en JavaScript 
 *
+*Las funciones en JavaScript no son muy diferentes a
+*las que manejamos en otros lenguajes, su principal objetivo
+*es centralizar las funciones del software en el que se
+*esta trabajando
 * */
+
